bcb3689af5341b06e57c6aec84fadbb87a294323
caoyingjunz <cao.yingjunz@gmail.com>
2023-02-28 21:25:59 +0800

    add

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	restful "github.com/emicklei/go-restful/v3"
 )
 
-func main() {
-	gin.SetMode(gin.DebugMode)
-	r := gin.Default()
+const (
+	apiPrefix        = "/scheduler"
+	bindPath         = apiPrefix + "/bind"
+	preemptionPath   = apiPrefix + "/preemption"
+	predicatesPrefix = apiPrefix + "/predicates"
+	prioritiesPrefix = apiPrefix + "/priorities"
+)
 
-	_ = r.Run(":8080")
+type Resource struct{}
+
+func (s Resource) WebService() *restful.WebService {
+	ws := new(restful.WebService)
+	ws.
+		Path(apiPrefix).
+		Consumes(restful.MIME_XML, restful.MIME_JSON).
+		Produces(restful.MIME_JSON, restful.MIME_XML)
+	//tags := []string{"scheduler"}
+
+	ws.Route(ws.GET("/version").To(s.version))
+
+	return ws
+}
+
+func (s Resource) version(req *restful.Request, res *restful.Response) {
+	res.WriteEntity(map[string]string{"version": "1.0.0"})
+}
+
+func main() {
+
 }
